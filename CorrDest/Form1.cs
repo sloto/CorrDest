d04834bbec1defa103d825e223d9a7a9aade41d9
sloto <rydm278@gmail.com>
2018-03-30 04:28:51 +0400

    Ability to set height on which viruses don't spawn added

--- CorrDest/Form1.cs
+++ CorrDest/Form1.cs
@@ -21,7 +21,7 @@ namespace CorrDest
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            SeedVirus(15);
+            SeedVirus(15 , 4);
         }
 
         public Form1()
@@ -58,11 +58,11 @@ namespace CorrDest
             g_static = Graphics.FromImage(pictureBox1.BackgroundImage);
             rng = new Random();
             virus_count = 15;
-            SeedVirus(virus_count);
+            SeedVirus(virus_count, 4);
 
 
         }
-        private void SeedVirus(int count) //это же по значению, верно? если сломается, то из-за этого
+        private void SeedVirus(int count, int free_space) //это же по значению, верно? если сломается, то из-за этого
         {
             g_static.DrawImage(new Bitmap(bg, pictureBox1.Size), 0, 0);
             for (int i = 0; i < v_x_amount; i++)
@@ -76,7 +76,7 @@ namespace CorrDest
             {
                 int type = rng.Next(type_amount); //возможно, стоит контролировать, чтобы было примерное одинаковое количество вирусов каждого типа
                 int i = rng.Next(v_x_amount);
-                int j = rng.Next(v_y_amount - 2);
+                int j = rng.Next(v_y_amount - free_space);
                 if (field[i, j] == -1)
                 {
                     count--;
@@ -87,18 +87,14 @@ namespace CorrDest
             
             for (int i = 0; i < v_x_amount; i++)
             {
-                for (int j = 0; j < v_y_amount - 2; j++)
+                for (int j = 0; j < v_y_amount; j++)
                 {
                     Rectangle current_block = new Rectangle(1 + i * v_width, pictureBox1.Height - (j + 1) * v_width - 1, v_width, v_width);
                     if (field[i, j] != -1)
                     {
                         g_static.DrawImage(blocks[field[i, j]], current_block);
                     }
-                    //g_static.DrawImage(blocks[rng.Next(type_amount)], i * v_width, pictureBox1.Height - j * v_width);
-
-                    //g_static.FillRectangle(pp[rng.Next(3)], current_block);
-                    //g_static.DrawImage(blocks[rng.Next(3)], current_block);
-                    g_static.DrawRectangle(new Pen(Color.Black), current_block);
+                    //g_static.DrawRectangle(new Pen(Color.Black), current_block);
                     
                 }
             }
